753ea045300898ca6ac371af4ef7f3e7c82b5083
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-07-29 17:29:33 +0700

    add migration and seeder for user

---
+++ database/migrations/2018_07_29_100954_user_table_migration.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTableMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('username', 20);
+            $table->string('password', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
